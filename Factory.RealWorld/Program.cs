b321f7a5e1aba4be2a05570b9405b4615b4b44e6
Enrico Rossini <enrico.rossini.uk@live.com>
2021-03-06 00:47:24 +0000

    First commit

---
+++ Factory.RealWorld/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Factory.RealWorld
+{
+    /// <summary>
+    /// Factory Design Pattern
+    /// </summary>
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Note: constructors call Factory Method
+            Document[] documents = new Document[2];
+
+            documents[0] = new Resume();
+            documents[1] = new Report();
+
+            // Display document pages
+            foreach (Document document in documents)
+            {
+                document.CreatePages();  // Factory method
+
+                Console.WriteLine("\n" + document.GetType().Name + "--");
+                foreach (Page page in document.Pages)
+                {
+                    Console.WriteLine(" " + page.GetType().Name);
+                }
+            }
+
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+
+    /// <summary>
+    /// The 'Product' abstract class
+    /// </summary>
+    public abstract class Page
+    {
+    }
+
+    /// <summary>
+    /// A 'ConcreteProduct' class
+    /// </summary>
+    public class SkillsPage : Page
+    {
+    }
+
+    /// <summary>
+    /// A 'ConcreteProduct' class
+    /// </summary>
+    public class EducationPage : Page
+    {
+    }
+
+    /// <summary>
+    /// A 'ConcreteProduct' class
+    /// </summary>
+    public class ExperiencePage : Page
+    {
+    }
+
+    /// <summary>
+    /// A 'ConcreteProduct' class
+    /// </summary>
+    public class IntroductionPage : Page
+    {
+    }
+
+    /// <summary>
+    /// A 'ConcreteProduct' class
+    /// </summary>
+    class ResultsPage : Page
+    {
+    }
+
+    /// <summary>
+    /// A 'ConcreteProduct' class
+    /// </summary>
+    public class ConclusionPage : Page
+    {
+    }
+
+    /// <summary>
+    /// A 'ConcreteProduct' class
+    /// </summary>
+    public class SummaryPage : Page
+    {
+    }
+
+    /// <summary>
+    /// A 'ConcreteProduct' class
+    /// </summary>
+    public class BibliographyPage : Page
+    {
+    }
+
+    /// <summary>
+    /// The 'Creator' abstract class
+    /// </summary>
+    public abstract class Document
+    {
+        List<Page> pages = new List<Page>();
+
+        public List<Page> Pages
+        {
+            get { return pages; }
+        }
+
+        // Factory Method
+        public abstract void CreatePages();
+    }
+
+    /// <summary>
+    /// A 'ConcreteCreator' class
+    /// </summary>
+    public class Resume : Document
+    {
+        // Factory Method implementation
+        public override void CreatePages()
+        {
+            Pages.Add(new SkillsPage());
+            Pages.Add(new EducationPage());
+            Pages.Add(new ExperiencePage());
+        }
+    }
+
+    /// <summary>
+    /// A 'ConcreteCreator' class
+    /// </summary>
+    public class Report : Document
+    {
+        // Factory Method implementation
+        public override void CreatePages()
+        {
+            Pages.Add(new IntroductionPage());
+            Pages.Add(new ResultsPage());
+            Pages.Add(new ConclusionPage());
+            Pages.Add(new SummaryPage());
+            Pages.Add(new BibliographyPage());
+        }
+    }
+}
---
+++ Factory.Structural/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace Factory.Structural
+{
+    /// <summary>
+    /// Factory Design Pattern
+    /// </summary>
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // An array of creators
+            Creator[] creators = new Creator[2];
+
+            creators[0] = new ConcreteCreatorA();
+            creators[1] = new ConcreteCreatorB();
+
+            // Iterate over creators and create products
+            foreach (Creator creator in creators)
+            {
+                Product product = creator.FactoryMethod();
+                Console.WriteLine("Created {0}",
+                    product.GetType().Name);
+            }
+
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+
+    /// <summary>
+    /// The 'Product' abstract class
+    /// </summary>
+    public abstract class Product
+    {
+    }
+
+    /// <summary>
+    /// A 'ConcreteProduct' class
+    /// </summary>
+    class ConcreteProductA : Product
+    {
+    }
+
+    /// <summary>
+    /// A 'ConcreteProduct' class
+    /// </summary>
+    public class ConcreteProductB : Product
+    {
+    }
+
+    /// <summary>
+    /// The 'Creator' abstract class
+    /// </summary>
+    public abstract class Creator
+    {
+        public abstract Product FactoryMethod();
+    }
+
+    /// <summary>
+    /// A 'ConcreteCreator' class
+    /// </summary>
+    public class ConcreteCreatorA : Creator
+    {
+        public override Product FactoryMethod()
+        {
+            return new ConcreteProductA();
+        }
+    }
+
+    /// <summary>
+    /// A 'ConcreteCreator' class
+    /// </summary>
+    public class ConcreteCreatorB : Creator
+    {
+        public override Product FactoryMethod()
+        {
+            return new ConcreteProductB();
+        }
+    }
+}
